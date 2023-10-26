80ea9707db584e97bc6cd96e8aa1562cb7fe2017
Jie Qiu <jieq@jies-mbp.lan>
2023-10-25 21:42:46 -0700

    seed script and hotel API

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-build:
+ build:
 	@go build -o bin/api
 
 run: build
 	@./bin/api --listenAddr :5010
+
+seed:
+	@go run scripts/seed.go
 
 test:
 	@go test -v ./...
--- main.go
+++ main.go
@@ -12,8 +12,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const dburi = "mongodb://localhost:27017"
-
 var config = fiber.Config{
 	ErrorHandler: func(c *fiber.Ctx, err error) error {
 		return c.JSON(map[string]string{"error": err.Error()})
@@ -21,36 +19,34 @@ var config = fiber.Config{
 }
 
 func main() {
-	listenAddr := flag.String("listenAddr", "5010", "The listrn address of the API server")
+
+	listenAddr := flag.String("listenAddr", "5010", "The listen address of the API server")
 	flag.Parse()
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// user := types.User{
-	// 	FirstName: "max",
-	// 	LastName: "verstappen",
-	// }
-	// newStore := db.NewMongoUserStore(client)
-	// res, err := newStore.GetColl().InsertOne(context.Background(), user)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(res)
-
 	// handlers initialization
-	userHandler := api.NewUserHandler(db.NewMongoUserStore(client, db.DBNAME))
+	var (
+		userHandler  = api.NewUserHandler(db.NewMongoUserStore(client, db.DBNAME))
+		hotelStore   = db.NewMongoHotelStore(client)
+		roomStore    = db.NewMongoRoomStore(client, hotelStore)
+		hotelHandler = api.NewHotelHandler(hotelStore, roomStore)
+		app          = fiber.New(config)
+		apiv1        = app.Group("/api/v1")
+	)
 
-	app := fiber.New(config)
-	apiv1 := app.Group("/api/v1")
-
+	// user handlers
 	apiv1.Post("/user", userHandler.HandlePostUser)
 	apiv1.Get("/user", userHandler.HandleGetUsers)
 	apiv1.Get("/user/:id", userHandler.HandleGetUserById)
 	apiv1.Delete("/user/:id", userHandler.HandleDeleteUserById)
 	apiv1.Put("/user/:id", userHandler.HandlePutUser)
+
+	// hotel handlers
+	apiv1.Get("/hotel", hotelHandler.HandleGetHotels)
 
 	// dereference the pointer to string
 	app.Listen(*listenAddr)
